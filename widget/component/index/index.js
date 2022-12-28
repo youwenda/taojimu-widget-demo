620a7e4004710f9e36c6ffbecd3dba83c9100cf1
jintai.yzq <jintai.yzq@taobao.com>
2022-12-28 19:20:27 +0800

    feat: 升级demo

--- widget/component/index/index.js
+++ widget/component/index/index.js
@@ -1,6 +1,10 @@
 import '@taojimu/widget-runtime';
 import polyfillToast from '../util/polyfillToast';
 
+const Mock = {
+  sellerId: '2247603103'
+};
+
 Component({
   mixins: [],
   data: {
@@ -8,7 +12,6 @@ Component({
     // toast
     showToast: {
       visible: false,
-      animationClass: '',
       type: '',
       content: ''
     },
@@ -16,39 +19,14 @@ Component({
   onInit() {
     console.log('onInit');
     polyfillToast.bind(this)();
-    this.getSceneInfo();
   },
   didMount() {
     console.log('onDidMount');
-    this.onGetItemDetail();
-    my.showToast({
-      type: 'success',
-      content: '我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子我是好孩子',
-      success() {
-        my.showToast({
-          type: 'fail',
-          content: '拉拉',
-          success() {
-            my.showLoading()
-          }
-        })
-      }
-    });
-  },
-  didUnmount() {
-    this.willUnmount = 1;
   },
   methods: {
-    setDataPromise(...args) {
-      return new Promise((resolve) => {
-        if (this.willUnmount) {
-          return;
-        }
-        this.setData(...args, () => resolve());
-      });
-    },
     getSceneInfo() {
       return new Promise((resolve, reject) => {
+        my.showLoading();
         my.getSceneInfo({
           success: res => {
             console.log('getSceneInfo', JSON.stringify(res));
@@ -56,12 +34,16 @@ Component({
           },
           fail: err => {
             reject(err);
+          },
+          complete: () => {
+            my.hideLoading();
           }
         });
       })
     },
     getConfigData() {
       return new Promise((resolve, reject) => {
+        my.showLoading();
         my.tb.getConfigData({
           success: res => {
             console.log('getConfig的返回值', res);
@@ -83,11 +65,17 @@ Component({
             if (!webapp) {
               return reject('getConfigData illegal');
             }
+            // 如何判断是否在可视化编辑模式下。
+            // 注意要判断TJM 这个对象是否存在，开发环境不存在这个变量。
+            const isIDE = webapp.TJM && webapp.TJM.isIDE;
             resolve(webapp);
           },
           fail: err => {
             console.log('getConfigData fail', err);
             reject('getConfigData fail')
+          },
+          complete: () => {
+            my.hideLoading();
           }
         });
       });
@@ -95,180 +83,123 @@ Component({
     onGetSceneInfo() {
       this.getSceneInfo().then(sceneInfo => {
         my.alert({
-          content: `success: ${JSON.stringify(sceneInfo)}`
+          content: `my.getSceneInfo success: ${JSON.stringify(sceneInfo)}`
         });
       }).catch(reason => {
         my.alert({
-          content: `fail: ${JSON.stringify(reason)}`
+          content: `my.getSceneInfo fail: ${JSON.stringify(reason)}`
         });
       });
     },
     onGetConfigData() {
       this.getConfigData()
       .then(configData => {
-
+        my.alert({
+          content: `my.getConfigData success: ${JSON.stringify(configData)}`
+        });
       })
       .catch(reason => {
         my.showToast({
-          content: `getconfigdata fail: ${JSON.stringify(reason)}`
+          content: `my.getConfigData fail: ${JSON.stringify(reason)}`
         });
       })
     },
-    onGetSceneId() {
-      this.getSceneInfo().then(({sceneInfo}) => {
-        my.alert({
-          content: `success: ${sceneInfo.sceneId}`
-        });
-      }).catch(reason => {
-        my.alert({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
-      });
+    onGotoDocs() {
+      my.showToast({
+        content: '需要申请外跳权限后，方可支持跳转，3s后执行跳转',
+        duration: 3e3,
+        success: () => {
+          my.tb.tjmNavigateToOutside({
+            url: 'https://mos.m.taobao.com/taojimu/docs#/widget/runtime',
+            succes() {
+              my.showToast({
+                content: '跳转成功'
+              });
+            },
+            fail: (res) => {
+              my.showToast({
+                content: `跳转失败，原因是: ${res.error}`
+              });
+            }
+          })
+        }
+      })
     },
-    onGetPageUrl() {
-      this.getSceneInfo().then(({sceneInfo}) => {
-        my.alert({
-          content: `success: ${sceneInfo.pageUrl}`
-        });
-      }).catch(reason => {
-        my.alert({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
-      });
-    },
-    onGetOrderId() {
-      this.getSceneInfo().then(({sceneInfo}) => {
-        my.alert({
-          content: `success: orderId: ${sceneInfo.orderId}, realOrderId: ${sceneInfo.realOrderId}`
-        });
-      }).catch(reason => {
-        my.alert({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
-      });
-    },
-    onGetPageConf() {
-      this.getSceneInfo().then(({sceneInfo}) => {
-        my.alert({
-          content: `success: ${sceneInfo.pageConf && JSON.stringify(sceneInfo.pageConf) || '暂时不存在pageConf'}`
-        });
-      }).catch(reason => {
-        my.alert({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
+    onShopFavor() {
+      my.tb.tjmShopFavor({
+        // 测试环境下可以随便指定一个sellerId, 发布线上后，sellerId 会被修正为制作页面的那个商家
+        sellerId: Mock.sellerId,
+        success: (res) => {
+          console.log('tjmShopFavor success ', res);
+          my.showToast({
+            content: '订阅成功！可在「淘宝首页-订阅」看TA的动态哦',
+          });
+        },
+        fail: (res) => {
+          // res = { error: string } // 订阅店铺失败原因
+          console.log('tjmShopFavor fail ', res);
+          my.showToast({
+            content: res.error
+          });
+        },
       });
     },
     onGetItemDetail() {
       this.getConfigData()
       .then(configData => {
-        const { scenes, items } = configData;
-        const itemId = scenes && scenes.item && scenes.item.length && scenes.item[0] || (items && items.length && items[0]) || '';
+        const { scenes } = configData;
+        const itemId = scenes && scenes.item && scenes.item.length && scenes.item[0];
         if (!itemId) {
-          return this.setData({
-            itemDetail: '配置应用未包含itemId'
-          })
+          return my.showToast({
+            content: '配置应用中未设置商品数据'
+          });
         }
+        const itemPrefetchData = scenes.itemPrefetchData[0];
         my.tb.tjmItemGetDetail({
-          ids: [itemId]
-        }).then(res => {
-          if (Array.isArray(res)) {
-            this.setData({
-              itemDetail: JSON.stringify(
-                Object.assign({
-                  auction_id: res[0].auction_id,
-                }, res[0].finalDisplayPrice)
-              )
+          ids: [itemId],
+          success: res => {
+            my.alert({
+              content: `宝贝${itemId}数据如下，各字段返回含义请参看文档：${JSON.stringify(res)}`
+            });
+          },
+          fail: res => {
+            my.showToast({
+              content: `宝贝${itemId}查询失败：${res.error}。开发者可以考虑使用预取数据(目前的demo存储在itemPrefetchData中)，作为商品的抄底展示，不过需要注意的是：预取数据在页面保存发布后不会更新。`
             });
           }
-        }).catch(err => {
-          console.log('my.tb.tjmItemGetDetail fail', JSON.stringify(err));
-          this.setData({
-            itemDetail: `宝贝${itemId}查询失败: ${JSON.stringify(err)}`
-          });
         });
-      })
-      .catch(reason => {
-        my.showToast({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
-      })
-    },
-    onGetStore() {
-      my.tb.tjmGetStore()
-      .then(({ data: store }) => {
-        this.setData({ store: JSON.stringify(store) });
-      })
-      .catch((reason) => {
-        console.log('getStore fail ', JSON.stringify(reason));
-      })
-    },
-    onSaveStore() {
-      const expando = (function() {
-        const date = new Date();
-        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(
-          date.getDate()
-        ).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(
-          date.getMinutes()
-        ).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
-      })();
-      my.tb.tjmSetStore({
-        data: {
-          expando
-        },
-        success: () => {
-          this.setData({ store: JSON.stringify({ expando }) });
-          my.alert({
-            content: `saveStore success: ${JSON.stringify({ expando }) }`,
-            success() {
-            }
-          });
-        },
-        fail(reason) {
-          my.alert({
-            content: `saveStore fail: ${JSON.stringify(reason)}`
-          });
-        }
-      })
+      });
     },
     onOpenDetail() {
-      my.alert({
-        content: my.tb.openDetail.toString(),
-        success: () => {
-          my.tb.openDetail({
-            itemId: '663452251907'
+      my.tb.openDetail({
+        itemId: '663452251907',
+        success: res => {
+          my.showToast({
+            content: '跳转成功'
+          });
+        },
+        fail: res => {
+          my.showToast({
+            content: `跳转失败, 原因是：${JSON.stringify(res)}`
           });
         }
       });
     },
-    onDrawPrize() {
-      this.getConfigData()
-      .then(configData => {
-        const { scenes } = configData;
-        const oright = scenes && scenes.oright;
-        if (!oright) {
+    onJoinMember() {
+      my.tb.tjmMemberJoin({
+        // 测试环境下可以随便指定一个sellerId, 发布线上后，sellerId 会被修正为制作页面的那个商家
+        sellerId: Mock.sellerId,
+        success: res => {
           my.showToast({
-            content: `没有选择奖池数据`
+            content: '已成功入会',
           });
-          return;
+        },
+        fail: res => {
+          my.showToast({
+            content: `入会失败，原因是: ${res.error}`
+          });
         }
-        my.tb.tjmDrawPrize({
-          ename: oright.ename,
-          type: oright.type,
-        }).then(res => {
-          my.alert({
-            content: `drawPrize success: ${JSON.stringify(res)}`
-          });
-        }).catch(res => {
-          my.alert({
-            content: `drawPrize fail: ${JSON.stringify(res)}`
-          });
-        });
-      })
-      .catch(reason => {
-        my.showToast({
-          content: `get ConfigData fail: ${JSON.stringify(reason)}`
-        });
-      })
+      });
     },
     onDrawCoupon() {
       this.getConfigData()
@@ -278,7 +209,7 @@ Component({
         const couponsPrefetchData = scenes && scenes.couponsPrefetchData;
         if (!coupons || !coupons.length || !couponsPrefetchData || !couponsPrefetchData.length) {
           my.showToast({
-            content: `没有选择优惠券数据`
+            content: `配置应用中未设置优惠券数据`
           });
           return;
         }
@@ -292,51 +223,74 @@ Component({
           supplierId,
           couponTag,
           terminals,
-        }).then(res => {
-          my.alert({
-            content: `成功领取${info}优惠券: ${JSON.stringify(res)}`
-          });
-        }).catch(res => {
-          my.alert({
-            content: `领取${info}优惠券失败: ${JSON.stringify(res)}`
-          });
-        });
-      })
-      .catch(reason => {
-        my.showToast({
-          content: `fail: ${JSON.stringify(reason)}`
-        });
-      })
-    },
-    onNavigation() {
-      my.tb.tjmGetRelationTaojimuUrl()
-      .then(res => {
-        my.alert({
-          content: `success: ${res.url}`,
-          success() {
-            my.tb.navigateToRelationTaojimuPage({
-              success: (res) => {
-                my.alert({
-                  content: `navigateToRelationTaojimuPage success: ${JSON.stringify(res)}`
-                });
-              },
-              fail: (res) => {
-                // res = { error: string } // 失败原因
-                console.log('navigateToRelationTaojimuPage fail ', res);
-                my.alert({
-                  content: `navigateToRelationTaojimuPage fail: ${JSON.stringify(res)}`
-                });
-              },
+          success: res => {
+            my.showToast({
+              content: `已成功领取${info}优惠券`
+            });
+          },
+          fail: res => {
+            my.showToast({
+              content: `领取${info}优惠券失败: ${res.error}`
             });
           }
         });
       })
       .catch(reason => {
-        console.log('tjmGetRelationTaojimuUrl fail ', reason);
-        my.alert({
-          content: `tjmGetRelationTaojimuUrl fail: ${JSON.stringify(reason)}`
+        my.showToast({
+          content: `获取配置数据错误: ${reason}`
         });
       });
+    },
+    onDrawPrize() {
+      this.getConfigData()
+      .then(configData => {
+        const { scenes } = configData;
+        const oright = scenes && scenes.oright;
+        if (!oright) {
+          my.showToast({
+            content: `配置应用中未设置奖池数据`
+          });
+          return;
+        }
+        my.tb.tjmDrawPrize({
+          ename: oright.ename,
+          type: oright.type,
+          succes: res => {
+            my.showToast({
+              content: `恭喜你中奖了：${JSON.stringify(res)}`
+            });
+          },
+          fail: res => {
+            my.showToast({
+              content: `很遗憾，您没有中奖：${res.error}`
+            });
+          }
+        });
+      })
+      .catch(reason => {
+        my.showToast({
+          content: `获取配置数据错误: ${JSON.stringify(reason)}`
+        });
+      });
+    },
+    onGetLiveLevel() {
+      my.tb.tjmLiveQueryLevel({
+        // // 测试环境下可以随便指定一个sellerId, 发布线上后，sellerId 会被修正为制作页面的那个商家
+        targetUid: Mock.sellerId,
+        success: (res) => {
+          my.alert({
+            content: `直播亲密度等级获取成功: ${JSON.stringify(res)}`
+          });
+        },
+        fail: (res) => {
+          my.showToast({
+            content: `直播亲密度等级获取成功: ${res.error}`
+          });
+        },
+      });
+    },
+    onGetTaskInfo() {
+
     },
     onShare() {
       my.showSharePanel({
@@ -353,25 +307,6 @@ Component({
           console.log('showSharePanel失败：', JSON.stringify(res));
         }
       });
-    },
-    onJoinMember() {
-      my.getSceneInfo({
-        success(res) {
-          const { sceneInfo } = res;
-          const { sellerId } = sceneInfo;
-          my.tb.tjmMemberJoin({
-            sellerId: '2064892827'
-          }).then(res => {
-            my.showToast({
-              content: `入会success: ${JSON.stringify(res)}`
-            });
-          }).catch(err => {
-            my.showToast({
-              content: `入会fail: ${JSON.stringify(err)}`
-            });
-          })
-        }
-      })
     },
     onFavorMiniapp() {
       this.getConfigData()
--- widget/component/util/polyfillToast.js
+++ widget/component/util/polyfillToast.js
@@ -3,96 +3,49 @@ const isFunction = (fn) => typeof fn === 'function';
 const delay = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
 export default function polyfillToast() {
   let promise = Promise.resolve();
-  my.showToast = ({ type, content, duration = 3000, success = noop, fail = noop, complete = noop }) => {
-    // promise = promise.then(() => {
-    //   return new Promise((resolve) => {
-    //     this.setData({
-    //       showToast: {
-    //         visible: true,
-    //         type,
-    //         content
-    //       }
-    //     }, () => {
-    //       const callback = () => {
-    //         try {
-    //           if (isFunction(success)) {
-    //             success();
-    //           }
-    //           if (isFunction(complete)) {
-    //             complete();
-    //           }
-    //         } catch(ex) {}
-    //         resolve();
-    //       };
-    //       if (duration > 0) {
-    //         delay(duration).then(() => {
-    //           this.setData({
-    //             showToast: {
-    //               visible: false,
-    //               type: '',
-    //               content: ''
-    //             }
-    //           }, () => {
-    //             callback();
-    //           });
-    //         });
-    //         return;
-    //       }
-    //       callback();
-    //     });
-    //   });
-    // });
-
-    promise = promise.then(() => this.setDataPromise({ showToast: { visible: true, type: '', animationClass: '', content: '' } }))
-    .then(() => delay(30))
-    .then(() => {
-      return this.setDataPromise({
-        showToast: {
-          type,
-          content,
-          visible: true,
-          animationClass: 'mx-toast-bd-show',
-        }
-      });
-    })
-    .then(() => {
-      return delay(Math.max(duration - 400, 0));
-    })
-    .then(() => {
-      if (duration > 0) {
-        return this.setDataPromise({
+  my.showToast = ({ type, content, duration = 2e3, success = noop, fail = noop, complete = noop } = { content: '' }) => {
+    promise = promise.then(() => {
+      return new Promise((resolve) => {
+        this.setData({
           showToast: {
-            type,
-            content,
             visible: true,
-            animationClass: 'mx-toast-bd-hide-v1',
+            type,
+            content
           }
-        })
-        .then(() => delay(400))
-        .then(() => {
-          return this.setDataPromise({
-            showToast: {
-              type,
-              content,
-              visible: false,
-              animationClass: 'mx-toast-bd-hide-v2',
+        }, () => {
+          const callback = () => {
+            try {
+              if (isFunction(success)) {
+                success();
+              }
+              if (isFunction(complete)) {
+                complete();
+              }
+            } catch(ex) {
             }
-          });
-        })
-        .then(() => {
+            resolve();
+          };
+          if (duration > 0) {
+            delay(duration).then(() => {
+              this.setData({
+                showToast: {
+                  visible: false,
+                  type: '',
+                  content: ''
+                }
+              }, () => {
+                console.log('trigger callback');
+                callback();
+              });
+            });
+            return;
+          }
+          callback();
         });
-      }
-    })
-    .then(() => {
-      console.log('toast 自动关闭');
-      if (isFunction(success)) {
-        return success();
-      }
+      });
     });
   };
-
   my.showLoading = ({ content = '', delay = 0, success = noop, fail = noop, complete = noop } = { content: '' }) => {
-    console.log('my.showLoading', typeof content, content);
     promise = promise.then(() => {
       return new Promise((resolve) => {
         this.setData({
@@ -135,7 +88,7 @@ export default function polyfillToast() {
   my.hideLoading = () => {
     promise = Promise.resolve();
     this.setData({
-      showLoading: {
+      showToast: {
         visible: false,
         type: '',
         content: ''
